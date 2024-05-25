0f334ace187b8ab08f2eae8d15515d4e2a81fef3
ACHREFGHO <72828687+ACHREFGHO@users.noreply.github.com>
2024-05-25 13:17:06 +0100

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
-
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    allowedHosts: ['dailysport.tn', 'www.dailysport.tn'],
-    disableHostCheck: true, // Disable host check to resolve the "Invalid Host Header" issue
+    allowedHosts: ['.dailysport.tn', 'dailysport.tn', 'www.dailysport.tn'],
   }
 })
+
